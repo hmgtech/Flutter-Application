ee266be4b22afaa546585d9330d7812bffc19d23
Hitesh Gupta <59255201+hmgtech@users.noreply.github.com>
2020-07-18 15:12:36 +0530

    Update home.dart
    File under constructons. Appliciation might crashed.

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:greenway_demo/main.dart';
 import 'entry.dart';
 
 class Home extends StatefulWidget {
@@ -10,9 +11,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   void entry()
   {
     Navigator.push(context, MaterialPageRoute(builder: (context)=>Entry()));    
+  }
+  Future signout() async
+  {
+    try
+    {
+      return await _auth.signOut();     
+    }
+    catch(e)
+    {
+      print(e.message);
+      return null;
+    }
   }
   @override
   
@@ -21,6 +35,20 @@ class _HomeState extends State<Home> {
        resizeToAvoidBottomPadding: false,
       appBar: AppBar(
         title: Text('Description'),
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text("Sign Out", style: TextStyle(fontSize: 18.0),),
+            onPressed: () async {
+              // dynamic result = await signout();
+              // if(result == null)
+              // {
+              //   Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginPage()  )   )   ;  
+              // }
+            } ,
+          ),
+          
+        ],
       ),
       body: Container(
         padding: EdgeInsets.only(top: 1.0, right: 20.0, left: 20.0, bottom: 20.0),
@@ -47,11 +75,16 @@ class _HomeState extends State<Home> {
               textColor: Colors.white,
               color: Colors.green,
               splashColor: Colors.cyan,),
+                          
+              
               ),
+              
           ]
           ),
       
       ),
     );
+  }
+}
   }
 }
